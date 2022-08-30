72e7ce73a2e2b88b34ce1c263d9bc7d6916b0222
vitorcruz <vitor-pcruz@hotmail.com>
2022-08-30 16:19:43 -0300

    defining configurations to acess db; migrations generated; fixing nullables attributes on classes

--- src/Endpoints/Categories/CategoryPost.cs
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -1,3 +1,4 @@
+using IWantApp.Domain.Products;
 using IWantApp.Infra.Data;
 
 namespace IWantApp.Endpoints.Categories;
@@ -10,7 +11,11 @@ public class CategoryPost
 
     public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
     {
-        return Results.Ok("Ok");
-    }
+        var category = new Category { Name = categoryRequest.Name };
 
+        context?.Categories?.Add(category);
+        context?.SaveChanges();
+
+        return Results.Created($"/categories/{category.Id}", category.Id);
+    }
 }
