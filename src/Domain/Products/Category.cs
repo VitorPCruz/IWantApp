3e0381af05f003454e4051065c669e6aafddbf2d
vitorcruz <vitor-pcruz@hotmail.com>
2022-08-30 12:01:56 -0300

    change string name as be nullable

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -1,8 +1,7 @@
-namespace IWantApp.Domain.Products
+namespace IWantApp.Domain.Products;
+
+public class Category
 {
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
+    public int Id { get; set; }
+    public string? Name { get; set; }
 }
