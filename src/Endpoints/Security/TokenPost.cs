db22a92ce567bbcfa9e3b69085a1ce74e22a5616
vitorcruz <vitor-pcruz@hotmail.com>
2022-09-05 17:42:55 -0300

    adding jwt bearer data to appsettings.json and configuring  default auth

--- src/Endpoints/Security/TokenPost.cs
+++ src/Endpoints/Security/TokenPost.cs
@@ -11,7 +11,7 @@ public class TokenPost
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(LoginRequest loginRequest, UserManager<IdentityUser> userManager)
+    public static IResult Action(LoginRequest loginRequest, IConfiguration configuration, UserManager<IdentityUser> userManager)
     {
         var user = userManager.FindByEmailAsync(loginRequest.Email).Result;
 
@@ -20,7 +20,7 @@ public class TokenPost
         if (!userManager.CheckPasswordAsync(user, loginRequest.Password).Result)
             return Results.BadRequest();
 
-        var key = Encoding.ASCII.GetBytes("A@fderwfQQSDXCCer34");
+        var key = Encoding.ASCII.GetBytes(configuration["JwtBearerTokenSettings:SecretKey"]);
 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
@@ -32,8 +32,8 @@ public class TokenPost
                 new SymmetricSecurityKey(key),
                 SecurityAlgorithms.HmacSha256Signature
             ),
-            Audience = "IWantApp",
-            Issuer = "Issuer"
+            Audience = configuration["JwtBearerTokenSettings:Audience"],
+            Issuer = configuration["JwtBearerTokenSettings:Issuer"]
         };
 
         var tokenHandler = new JwtSecurityTokenHandler();
