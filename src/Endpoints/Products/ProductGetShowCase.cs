4535d7cc18a3210750551fc0d00a6f2926e08cfa
vitorcruz <vitor-pcruz@hotmail.com>
2022-09-08 07:39:19 -0300

    add pagination to showcase

--- src/Endpoints/Products/ProductGetShowCase.cs
+++ src/Endpoints/Products/ProductGetShowCase.cs
@@ -7,13 +7,27 @@ public class ProductGetShowCase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static IResult Action(ApplicationDbContext context)
+    public static IResult Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
     {
-        var products = context.Products
-            .Include(p => p.Category)
-            .Where(p => p.HasStock && p.Category.Active)
-            .OrderBy(p => p.Name)
-            .ToList();
+        if (page == null)
+            page = 1;
+
+        if (row == null)
+            row = 1;
+
+        if (string.IsNullOrEmpty(orderBy))
+            orderBy = "name";
+
+        var queryBase = context.Products.Include(p => p.Category).Where(p => p.HasStock && p.Category.Active);
+
+        if (orderBy == "name")
+            queryBase = queryBase.OrderBy(p => p.Name);
+        else
+            queryBase = queryBase.OrderBy(p => p.Price);
+
+        var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+
+        var products = queryFilter.ToList();
 
         var results = products.Select(p => new ProductResponse(
             p.Name,
