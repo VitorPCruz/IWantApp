22ac6c3fec140fcfefdab178d0f3d3f4d69d3331
vitorcruz <vitor-pcruz@hotmail.com>
2022-09-08 07:46:55 -0300

    configuring validation at showcase pagination

--- src/Endpoints/Products/ProductGetShowCase.cs
+++ src/Endpoints/Products/ProductGetShowCase.cs
@@ -7,25 +7,20 @@ public class ProductGetShowCase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static IResult Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
+    public static IResult Action(ApplicationDbContext context, int page = 1, int row = 10, string orderBy = "name")
     {
-        if (page == null)
-            page = 1;
-
-        if (row == null)
-            row = 1;
-
-        if (string.IsNullOrEmpty(orderBy))
-            orderBy = "name";
+        if (row > 10) return Results.Problem(title: "Row with max 10", statusCode: 400);
 
         var queryBase = context.Products.Include(p => p.Category).Where(p => p.HasStock && p.Category.Active);
 
         if (orderBy == "name")
             queryBase = queryBase.OrderBy(p => p.Name);
-        else
+        else if (orderBy == "price")
             queryBase = queryBase.OrderBy(p => p.Price);
+        else
+            return Results.Problem(title: "Order only by price or name", statusCode: 400);
 
-        var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+        var queryFilter = queryBase.Skip((page - 1) * row).Take(row);
 
         var products = queryFilter.ToList();
 
