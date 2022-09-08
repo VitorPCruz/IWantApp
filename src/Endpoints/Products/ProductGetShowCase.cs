9eeb3e28fbb5a0947093e6d7957ff2251550739a
vitorcruz <vitor-pcruz@hotmail.com>
2022-09-08 07:50:12 -0300

    adding ef AsNoTracking() at showcase to improve performance

--- src/Endpoints/Products/ProductGetShowCase.cs
+++ src/Endpoints/Products/ProductGetShowCase.cs
@@ -11,7 +11,10 @@ public class ProductGetShowCase
     {
         if (row > 10) return Results.Problem(title: "Row with max 10", statusCode: 400);
 
-        var queryBase = context.Products.Include(p => p.Category).Where(p => p.HasStock && p.Category.Active);
+        var queryBase = context.Products
+            .AsNoTracking()
+            .Include(p => p.Category)
+            .Where(p => p.HasStock && p.Category.Active);
 
         if (orderBy == "name")
             queryBase = queryBase.OrderBy(p => p.Name);
